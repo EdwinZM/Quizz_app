7cf2dc8d4d474075110ff490daf61cf6d3fa4f06
Edwin Zwanenburg <edwinzwanenburg@Macs-MacBook-Pro.local>
2021-04-27 14:30:40 -0500

    answer checking implementation

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 THEME_COLOR = "#375362"
 import tkinter as tk
+from quiz_brain import QuizBrain
 
 class QuizInterface:
     def __init__(self, question):
@@ -13,19 +14,50 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(150, 125, width=280, text="Some question", font=("Arial", 20, "italic"))
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
         self.true_image = tk.PhotoImage(file="./images/true.png")
-        self.correct_button = tk.Button(image=self.true_image, highlightthickness=False)
+        self.correct_button = tk.Button(image=self.true_image, highlightthickness=False, command=self.return_true)
         self.correct_button.grid(column=0, row=2)
         self.false_image = tk.PhotoImage(file="./images/false.png")
-        self.correct_button = tk.Button(image=self.false_image, highlightthickness=False)
-        self.correct_button.grid(column=1, row=2)
+        self.false_button = tk.Button(image=self.false_image, highlightthickness=False, command=self.return_false)
+        self.false_button.grid(column=1, row=2)
         self.get_next_question()
+
 
         self.window.mainloop()
 
     def get_next_question(self):
-        new_question = self.question.next_question()
-        self.canvas.itemconfig(self.question_text, text=new_question)
+        try:
+            new_question = self.question.next_question()
+            self.canvas.itemconfig(self.question_text, text=new_question)
+            self.score_label.config(text=f"Score: {self.question.score}")
+            self.canvas.configure(bg="white")
+        except IndexError:
+            text = f"You've completed the quiz\nYour final score was: {self.question.score}/{self.question.question_number}"
+            self.canvas.itemconfig(self.question_text, text=text)
+            self.canvas.configure(bg="white")
+            self.correct_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
 
+    def return_true(self):
+        user_answer = "true"
+        result = self.question.check_answer(user_answer)
+        self.get_result(result)
+
+    def return_false(self):
+        user_answer = "false"
+        result = self.question.check_answer(user_answer)
+        self.get_result(result)
+        
+        
+    
+    def get_result(self, result):
+        if result:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.get_next_question)
+
+    
 
 
